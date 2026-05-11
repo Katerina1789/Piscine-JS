0b87ac25fed0a2b7f33194abd220b1a2d458a5c8
kkasdana <kadkat1789@gmail.com>
2026-05-11 11:14:24 +0300

    Created constant variables

---
+++ primitives.js
@@ -0,0 +1,11 @@
+// constant string value
+const str = 'Biba Buba';
+
+// constant number value
+const num = 1510; 
+
+// constant boolean value
+const bool = true;
+
+// constant undefined value (no assigned value)
+const undef = undefined;
