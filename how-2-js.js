2f4833a6fda15be8be3c4df1e388d2b8ec739412
kkasdana <kadkat1789@gmail.com>
2026-05-11 11:29:08 +0300

    Added theory comments for learning purposes

--- how-2-js.js
+++ how-2-js.js
@@ -1,1 +1,3 @@
+// this line prints text to the browser console
+// console.log is the simplest way to test JS code
 console.log('Hello World')
--- primitives.js
+++ primitives.js
@@ -1,10 +1,13 @@
 // constant string value
+// use " " if the string contains a ' -> 2 bytes per character (UTF-16) + emojis = 4 bytes
 const str = 'Biba Buba';
 
 // constant number value
+// JS has only one number type (integers + floats) aka 64‑bit IEEE 754 double -> 8 bytes
 const num = 1510; 
 
 // constant boolean value
+// used for conditions, comparisons, logic -> engine‑dependent byte count (in Go it is 1 byte)
 const bool = true;
 
 // constant undefined value (no assigned value)
