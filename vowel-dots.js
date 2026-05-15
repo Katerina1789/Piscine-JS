112518b3d767bd7f9fa82b00d17bad777d378c92
kkasdana <kadkat1789@gmail.com>
2026-05-16 01:31:50 +0300

    Vowel dots: changed stored variable

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,9 +1,9 @@
-// regex character set: matches any vowel a, e, i, o, u
-// without regex we would loop through chars and check manually
-// good to remember: replace() can insert matched text using $&
+// regex character set: matches any vowel a, e, i, o, u (both lower and upper case)
+// without regex we would loop through chars and check each against a vowel list
+// good to remember: $& in replace() means "the whole matched text"
 
-// regex stored in variable as required
-export const vowels = /[aeiou]/g
+// regex stored in variable 
+export const vowels = /[aeiouAEIOU]/g
 
 // function: adds a '.' after every vowel in the string
 export function vowelDots(str) {
@@ -15,17 +15,20 @@ export function vowelDots(str) {
 /*
 TESTING:
 console.log('--- basic tests ---')
-console.log(vowelDots('hello'))          // h.e.ll.o.
-console.log(vowelDots('example'))        // e.x.a.m.p.l.e.
-console.log(vowelDots('aeiou'))          // a.e.i.o.u.
+console.log(vowelDots('hello'))           // h.e.ll.o.
+console.log(vowelDots('example'))         // e.x.a.m.p.l.e.
+console.log(vowelDots('aeiou'))           // a.e.i.o.u.
+
+console.log('--- uppercase tests ---')
+console.log(vowelDots('Algorithm'))       // A.lgo.ri.thm
+console.log(vowelDots('AEIOU'))           // A.E.I.O.U.
 
 console.log('--- edge cases ---')
-console.log(vowelDots(''))               // ''
-console.log(vowelDots('bcdfg'))          // 'bcdfg' (no vowels)
-console.log(vowelDots('A E I O U'))      // 'A E I O U' (uppercase not matched)
+console.log(vowelDots(''))                // ''
+console.log(vowelDots('bcdfg'))           // 'bcdfg' (no vowels)
 
 console.log('--- mixed cases ---')
-console.log(vowelDots('you are amazing')) // y.o.u. a.r.e. a.m.a.z.i.n.g.
+console.log(vowelDots('You are Amazing')) // Yo.u. a.re. A.ma.zi.ng.
 
 run in Terminal: node vowel-dots.js
 */
