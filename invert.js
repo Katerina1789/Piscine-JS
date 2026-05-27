503226e4634bb8b611d7241fd6d3351518b67ce2
kkasdana <kadkat1789@gmail.com>
2026-05-27 22:03:31 +0300

    Learned Object commands

--- invert.js
+++ invert.js
@@ -1,18 +1,10 @@
-// invert returns a new object with keys and values swapped
-function invert(obj) {
-  const result = {} // new object to store inverted pairs
+// Object.entries(): returns an array of [key, value] pairs from an object's own properties
+// Object.fromEntries(): builds an object from an array of [key, value] pairs
 
-  for (const key in obj) {
-    const value = obj[key]
-    result[value] = key // assign inverted pair
-  }
-
-  return result
-}
-
-/*
-Testing:
-console.log(invert({ a: 1, b: 2 }))           // { '1': 'a', '2': 'b' }
-console.log(invert({ x: 'hello', y: 'bye' })) // { hello: 'x', bye: 'y' }
-console.log(invert({ 1: 'one', 2: 'two' }))   // { one: '1', two: '2' }
-*/
+// invert returns a new object with keys and values swapped, last duplicate value wins
+export const invert = (obj) => {
+  // swap each [key, value] pair to [value, key] then build a new object from the result
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [value, key])
+  );
+};
